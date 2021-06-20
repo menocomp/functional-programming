17d5c745fd3fcecfd3eaf8476648aa4798e48485
Mina Luke <mina@sine.co>
2021-06-20 15:25:44 +0930

    strict props to string based only

--- examples/compose.ts
+++ examples/compose.ts
@@ -2,15 +2,21 @@ import { compose } from "../src";
 
 interface IPerson {
   name: string;
-  address: string;
+  address: string
+  age: number;
+  isMale: boolean;
 }
 
 const person: IPerson = {
   name: "Mina",
   address: "xyz",
+  age: 30,
+  isMale: true,
 };
 
-const getProp = (person: IPerson, prop: keyof IPerson) => person[prop];
+type PropsWithType<T, U> = keyof { [P in keyof T as T[P] extends U ? P : never]: T[P] };
+
+const getProp = <T extends IPerson, U extends keyof T & PropsWithType<T, string>>(person: T, prop: U): T[U] => person[prop];
 const getName = (person: IPerson) => person.name;
 const getLength = (str: string) => str.length;
 const isEven = (num: number) => num % 2 === 0;
